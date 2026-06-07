72319ab8237c841c0e747cb016b75828937484c0
asus <ajiseno.suseno@gmail.com>
2026-06-07 23:31:32 +0700

    Refactory link page CONSUMER

--- Object Repository/Consumer.rs
+++ Object Repository/Consumer.rs
@@ -26,7 +26,7 @@
    <migratedVersion>5.4.1</migratedVersion>
    <path></path>
    <restRequestMethod>GET</restRequestMethod>
-   <restUrl>https://reqres.in/api/users/2</restUrl>
+   <restUrl>https://reqres.in/api/users?page=2</restUrl>
    <serviceType>RESTful</serviceType>
    <soapBody></soapBody>
    <soapHeader></soapHeader>
